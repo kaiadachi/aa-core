e02732179f5c4b645ccb64e77bba7a88c2eb7e70
kaiadachi <kaikaikai8217@gmail.com>
2023-11-30 21:09:33 +0900

    add dist

--- dist/cjs/provider/base.js
+++ dist/cjs/provider/base.js
@@ -295,7 +295,7 @@ class SmartAccountProvider extends eventemitter3_1.default {
                 const initCode = await this.account.getInitCode();
                 return this._runMiddlewareStack({
                     initCode,
-                    sender: this.getAddress(),
+                    sender: "0x6b4B94617D319f818486f2EdDDbBF4EF58365063",
                     nonce: this.account.getNonce(),
                     callData: Array.isArray(data)
                         ? this.account.encodeBatchExecute(data)
--- src/provider/base.ts
+++ src/provider/base.ts
@@ -358,7 +358,7 @@ export class SmartAccountProvider<
     return this._runMiddlewareStack(
       {
         initCode,
-        sender: this.getAddress(),
+        sender: "0x6b4B94617D319f818486f2EdDDbBF4EF58365063",
         nonce: this.account.getNonce(),
         callData: Array.isArray(data)
           ? this.account.encodeBatchExecute(data)
